80afbd80de89ac94a05532f74b79d9d1a335aa6c
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-07 14:47:54 -0400

    Updated url in config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://shielded-retreat-80372.herokuapp.com/',
+  production: 'https://shielded-retreat-80372.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
