4be898d657798badd9e258e1af854791093763f6
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-07 09:02:14 -0400

    Styling

--- assets/scripts/post/ui.js
+++ assets/scripts/post/ui.js
@@ -4,51 +4,61 @@ const showPostTemplate = require('../templates/allPost.handlebars')
 const onePostTemplate = require('../templates/onePost.handlebars')
 
 const createPostSuccess = function (response) {
-  store.user = response.user
+  store.post = response.post
+  $('#view-posts').empty()
   $('#message').text('You submitted a post')
   $('form').trigger('reset')
 }
 
 const createPostFailure = function (response) {
+  $('#view-posts').empty()
   $('#message').text('try again')
   $('form').trigger('reset')
 }
 
 const showAllSuccess = function (response) {
+  $('#view-posts').empty()
   $('#message').text(`There are ${response.posts.length} posts`)
   const showAllPost = showPostTemplate({ posts: response.posts })
   $('#view-posts').append(showAllPost)
 }
 
 const showAllFailure = function (response) {
+  $('#view-posts').empty()
   $('#message').text('try again')
 }
 
 const showOneSuccess = function (response) {
+  $('#view-posts').empty()
   $('#message').text('it works')
   const showOnePost = onePostTemplate({ post: response.post })
   $('#view-posts').append(showOnePost)
 }
 
 const showOneFailure = function (response) {
+  $('#view-posts').empty()
   $('#message').text('try again')
 }
 
 const deleteOneSuccess = function (response) {
+  $('#view-posts').empty()
   $('#message').text('Resource deleted')
   $('form').trigger('reset')
 }
 
 const deleteOneFailure = function (response) {
+  $('#view-posts').empty()
   $('#message').text('try again')
 }
 
 const updateSuccess = function (response) {
+  $('#view-posts').empty()
   $('#message').text('Resource update')
   $('form').trigger('reset')
 }
 
 const updateFailure = function (response) {
+  $('#view-posts').empty()
   $('#message').text('try again')
 }
 
