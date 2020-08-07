48fd655b799f68374b6fc58e6bfbc42930c8c92c
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-07 13:05:13 -0400

    Updated config
    added deployed api url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://shielded-retreat-80372.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
